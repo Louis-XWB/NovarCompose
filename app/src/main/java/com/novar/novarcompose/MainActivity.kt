3524e000203d5f7712e42422957fafed7c48c8dc
Novar <xuwab77@gmail.com>
2023-10-28 12:32:17 +0800

    add drawable assets

--- app/src/main/java/com/novar/novarcompose/MainActivity.kt
+++ app/src/main/java/com/novar/novarcompose/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
 import com.novar.novarcompose.ui.theme.NovarComposeTheme
 
 class MainActivity : ComponentActivity() {
